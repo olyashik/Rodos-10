e1ff279a0b0887cebeca3a57589ab85d1ced55a6
olyashik <102978625+olyashik@users.noreply.github.com>
2025-12-11 15:44:40 +0300

    Update RODOS.py

--- RODOS.py
+++ RODOS.py
@@ -1,10 +1,22 @@
 from conditions import *
 
+# Основное тело
 if __name__ == "__main__":
-    print("Hi!")
-    condition()
-    
-                
-    
+    while True:
+        # Сначала вводится номер реле, потом команда
+        try:
+            memory = input().split()
+            numb_rel = memory[0]
+            command = memory[1]
+            c_list = ['s', 'n', 'f']
+            n_list = ['0', '1', '2', '3']
+            if (numb_rel in n_list) and (command in c_list):
+                condition(numb_rel, command)
+            else:
+                print("Введено неверное значение")
 
+        except Exception as e:
+            print("Ошибка")
+
+    
     
